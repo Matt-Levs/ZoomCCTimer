8df28e1748bdf89b5b7d8ebb87ca30043a783fc6
mlevins <mdlevins@rogers.com>
2022-03-13 23:27:34 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# ZoomCCTimer
-A timer app that posts updates of the time as a Closed Caption in a Zoom meeting
+## ZoomCCTimer
+
+A timer app that posts updates of the time as a Closed Caption in a Zoom meeting. This program takes advantage of Zoom's third party closed captioning APIKey to display a timer for all participents in a meeting who enable their closed captioning service.
+
+**Motivation:**
+The purpose of this was to create a minimalist timer for any Zoom user without the need for a paid plugin or having the need share their screen that has a timer on it. This program is great for scenarios where a meeting attendee needs to share their screen for a presentation but everyone needs to be aware of the time.
+
+**Build Status:**
+Current Features:
+- Stopwatch (Count Up) Timer with 'Stop' and 'Reset' functions for local timing
+- APIKey testing button to check if the correct URL has been pasted in the entry field
+- Time is posted as a Closed Caption every 30 seconds (Hard coded)
+
+Bugs:
+- Timer doesnt run without a valid API key
+  - Fails when reaching 30 sec due to fiald POST function
+  
+Future Features:
+- Adjustable POST interval (Every 30sec, 1 min, 5 min, 10 min)
+- Switch between count-down vs count-up timer
+  - Count-down: Set to desired minutes and countdown, send POST when time is reached, continue into "Negative Time" to show overtime amount
+- Variable countdown POST interval (every 5min then with 5 min remaining, POST every min, then every 10 sec, then time up)
+- Initial screen in window showing instructions for obtaining APIKey 
+  - Can also be a simple screen with a URL to the GIThub instructions page
+- Future replication of the program onto a webserver
+  - Users can either download the program or go to a webpage that has the app
+
+# Development Notes
+
+**Building the App**
+This app uses pyinstaller to create a .exe file for the program to run on the users computer. The current build can be found in dist/. To build a new .exe file the command is:
+
+pyinstaller --onefile .\ZoomCCTimer.py
+
+
+# Resources/References
+**Zoom CC API**
+https://support.zoom.us/hc/en-us/articles/115002212983-Using-a-third-party-closed-captioning-service
+
+**How to Enable Zoom Closed Captioning in Meetings**
+https://support.zoom.us/hc/en-us/articles/4409683389709
---
+++ ZoomCCTimer.py
@@ -0,0 +1,114 @@
+import tkinter as tk
+import requests
+
+root= tk.Tk()
+canvas1 = tk.Canvas(root, width = 400, height = 300)
+canvas1.pack()
+
+apiURLAdd_1 = '&seq='
+apiURLAdd_2 = '&lang=en-US'
+
+m=0; s=0
+seqVal = 0
+
+stop_flag=False
+sendBool = False
+newClock = True
+
+reset_txt=f"00:00"
+
+def sendUpdate(seqCounter, apiKeyValue):
+    url = apiKeyValue+apiURLAdd_1+str(seqCounter)+apiURLAdd_2
+    headers = {'Content-type': 'text/plain'}
+    x = requests.post(url, headers=headers, data = 'Elapsed Time: ' + clock_lbl['text'] + '\n') 
+    if 200 <= x.status_code <= 299:
+        return True
+    else:
+        return False
+
+def getLastSeq():
+    global seqVal
+    apiKeyValue = apiKey.get()
+    if apiKeyValue.startswith('http'):
+        index = apiKeyValue.find('?')
+        url = apiKeyValue[:index] + '/seq' + apiKeyValue[index:]
+        x = requests.get(url)
+        if 200 <= x.status_code <=299:
+            check_btn['text'] = 'Success'
+            check_btn['bg'] = 'green'
+            check_btn['fg'] = 'white'
+            seqVal = int(x.text)
+        else:
+            check_btn['text'] = 'Fail'
+            check_btn['bg'] = 'red'
+            check_btn['fg'] = 'white'
+    else:
+        check_btn['text'] = 'Fail'
+        check_btn['bg'] = 'red'
+        check_btn['fg'] = 'white'
+
+
+def clock_run():
+    global m, s, sendBool, seqVal, apiKeyValue
+    if not stop_flag:
+        s+=1
+        if s ==30:
+            sendBool = True
+        elif s == 60:
+            m+=1; s=0
+            sendBool = True
+
+        clock_lbl['text']=f"{m:02}:{s:02}"
+
+        if sendBool:
+            seqVal += 1
+            if not sendUpdate(seqVal, apiKeyValue):
+                getLastSeq()
+                seqVal += 1
+                if not sendUpdate(seqVal, apiKeyValue):
+                    print('Failed to Send Update')
+                sendBool = False
+            else:
+                sendBool = False
+        canvas1.after(1000, clock_run)
+
+def  Start():
+    global stop_flag, apiKeyValue, newClock
+    apiKeyValue = apiKey.get()
+    stop_flag=False
+    if newClock:
+        clock_run()
+        newClock = False
+
+def Stop():
+    global stop_flag
+    stop_flag=True 
+
+
+def reset1(event=None):
+    global s, m, stop_flag, newClock
+    s=0; m=0
+    clock_lbl['text']=reset_txt
+    stop_flag=True
+    newClock = True
+
+
+apiLabel = tk.Label(root, text= 'API Key')
+canvas1.create_window(200, 20, window=apiLabel)
+apiKey = tk.Entry(root)
+canvas1.create_window(200, 50, window=apiKey)
+check_btn = tk.Button(master=canvas1, text="Check API Key", width=20, command=getLastSeq)
+canvas1.create_window(200, 80, window=check_btn)
+
+
+clock_lbl = tk.Label(master=canvas1, height=2, width=10,text=reset_txt,font=(None, 20))
+canvas1.create_window(200, 150, window=clock_lbl)
+
+start_btn = tk.Button(master=canvas1, text="START", width=10, command=Start, bg='green', fg='white')
+canvas1.create_window(75, 200, window=start_btn)
+stop_btn = tk.Button(master=canvas1, text="STOP", width=10, command=Stop, bg='red', fg='white')
+canvas1.create_window(200, 200, window=stop_btn)
+reset_btn = tk.Button(master=canvas1, text="RESET",width=10,command = reset1, bg='yellow')
+canvas1.create_window(325, 200, window=reset_btn)
+
+root.mainloop()
